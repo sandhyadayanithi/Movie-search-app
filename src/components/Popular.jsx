5f937958b01f1349921d9c9ad95575abe9a3b79b
Sandhya <sandhyadayanithi@gmail.com>
2026-01-18 20:07:31 +0530

    Added error handling for data fetching

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -7,17 +7,32 @@ const Popular = () => {
   const [search,setSearch]=useState("");
 
   async function fetchMovies(){
-    const url=`https://api.themoviedb.org/3/movie/popular?api_key=${YOUR_API_KEY}`;
-    const res=await fetch(url);
-    const data=await res.json();
-    setMovieData(data.results);
+    try{
+      const url=`https://api.themoviedb.org/3/movie/popular?api_key=${YOUR_API_KEY}`;
+      const res=await fetch(url);
+      const data=await res.json();
+      setMovieData(data.results);
+    }
+    catch(err){
+      console.log('Error fetching data.');
+    }
   }
 
   async function searchMovies(){
-    const url=`https://api.themoviedb.org/3/search/movie?api_key=${YOUR_API_KEY}&query=${search}`;
-    const res=await fetch(url);
-    const data=await res.json();
-    setMovieData(data.results);
+    if (search){
+      try{
+        const url=`https://api.themoviedb.org/3/search/movie?api_key=${YOUR_API_KEY}&query=${search}`;
+        const res=await fetch(url);
+        const data=await res.json();
+        setMovieData(data.results);
+      }
+      catch(err){
+        console.log('Error fetching data.');
+      }
+    }
+    else{
+      console.log('No query to search.');
+    }
   }
 
   function inputHandler(e){
@@ -39,7 +54,7 @@ const Popular = () => {
     <div>
       <div className='flex justify-center items-center gap-6 mb-8'>
         <input onChange={inputHandler} className='border-2 border-green-300 px-4 py-1' placeholder='Search for a movie...'></input>
-        <button onClick={searchMovies} className='px-3 py-1 rounded bg-green-300 text-gray-800'>Search</button>
+        <button onClick={searchMovies} className='px-3 py-1 rounded bg-green-300 text-gray-800 cursor-pointer'>Search</button>
       </div>
       <div className='grid gap-5 grid-cols-2 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 shrink-2 pb-5'>
         {movieData?.map((movie)=>{
